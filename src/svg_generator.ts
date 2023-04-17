1652e80a7087fa14d119588e1358e5ef2d00a307
Dominic Seithel <seitheldominic@gmail.com>
2023-04-17 16:20:30 +0200

    board with text and colors

--- src/svg_generator.ts
+++ src/svg_generator.ts
@@ -11,7 +11,11 @@ class SVGGenerator {
         this.addElement('rect', {x: x, y: y, width: width, height: height, style: style});
     }
 
-    private addElement(form: string, attributes: { [index: string]: any }): object {
+    public addText(x: number, y: number, content: string, style: string): void {
+        this.addElement('text', {x: x, y: y, style: style}).innerHTML = content;
+    }
+
+    private addElement(form: string, attributes: { [index: string]: any }): SVGElement {
         let svgBase = document.createElementNS('http://www.w3.org/2000/svg', form);
         for(const key in attributes) svgBase.setAttribute(key, attributes[key]);
         return this.svg.appendChild(svgBase);
