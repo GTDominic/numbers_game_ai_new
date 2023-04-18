0848923d7638fea115e4b1b4fd51183d8dbc0ded
Dominic Seithel <seitheldominic@gmail.com>
2023-04-18 11:12:28 +0200

    auto run

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 let currentAI: AIAbstract;
+let aiAutoRunSpeed = 500;
+const aiAutoRun = setInterval(aiRunAuto, aiAutoRunSpeed);
 
 function main(): void {
     currentAI = new AIFirstFound();
@@ -6,4 +8,9 @@ function main(): void {
 
 function aiStep(): void {
     currentAI.step();
+}
+
+function aiRunAuto(): void {
+    let element = <HTMLInputElement> document.getElementById('autoAI');
+    if(element.checked) currentAI.step();
 }
