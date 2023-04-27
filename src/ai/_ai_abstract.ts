abb57c373eabce9f3e0277f1ee6ac42b1f05afa1
Dominic Seithel <seitheldominic@gmail.com>
2023-04-27 10:04:02 +0200

    prettier

--- src/ai/_ai_abstract.ts
+++ src/ai/_ai_abstract.ts
@@ -3,13 +3,13 @@ abstract class AIAbstract {
     protected finished: boolean = false;
 
     constructor() {
-        let startvalueselement = <HTMLInputElement> document.getElementById('startvalues');
-        let startelvalue: string = startvalueselement.value.replace(/ /g, ",");       
-        let autoDelete = <HTMLInputElement> document.getElementById('autoDeleteRows');
+        let startvalueselement = <HTMLInputElement>document.getElementById("startvalues");
+        let startelvalue: string = startvalueselement.value.replace(/ /g, ",");
+        let autoDelete = <HTMLInputElement>document.getElementById("autoDeleteRows");
         let startvalues: Array<number> = JSON.parse("[" + startelvalue + "]");
         this.board = new Board(startvalues, autoDelete.checked);
     }
-    
+
     /**
      * Executes a single step
      */
@@ -23,12 +23,12 @@ abstract class AIAbstract {
     }
 
     protected checkOrFinished(): void {
-        if(this.finished) return;
-        if(this.board.checkFinished()) {
-            document.getElementById('textout').innerHTML = '<p class="w3-padding-large">Board finished!</p>';
+        if (this.finished) return;
+        if (this.board.checkFinished()) {
+            document.getElementById("textout").innerHTML = '<p class="w3-padding-large">Board finished!</p>';
             this.finished = true;
             return;
         }
-        this.board.check()
+        this.board.check();
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,10 @@ let calls = 0;
 const aiAutoRun = setInterval(aiRunAuto, config.aiAutoRunSpeed);
 
 function selectAI(): void {
-    let element = <HTMLInputElement> document.getElementById('selectedAI');
+    let element = <HTMLInputElement>document.getElementById("selectedAI");
     let ai = element.value;
-    if(ai === 'first_found') currentAI = new AIFirstFound();
-    document.getElementById('textout').innerHTML = '';
+    if (ai === "first_found") currentAI = new AIFirstFound();
+    document.getElementById("textout").innerHTML = "";
 }
 
 function aiStep(): void {
@@ -18,13 +18,13 @@ function deleteRows(): void {
 }
 
 function aiRunAuto(): void {
-    let element = <HTMLInputElement> document.getElementById('autoAI');
-    let slowElement = <HTMLInputElement> document.getElementById('slowAutoAI');
-    if(calls === 1000000000000) calls = 0;
+    let element = <HTMLInputElement>document.getElementById("autoAI");
+    let slowElement = <HTMLInputElement>document.getElementById("slowAutoAI");
+    if (calls === 1000000000000) calls = 0;
     calls++;
-    if(element.checked) {
-        if(slowElement.checked) {
-            if(calls % config.aiAutoRunSpeedModifier === 0) currentAI.step();
+    if (element.checked) {
+        if (slowElement.checked) {
+            if (calls % config.aiAutoRunSpeedModifier === 0) currentAI.step();
         } else currentAI.step();
     }
 }
