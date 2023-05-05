3551a4180f5ba0e9ea6267ecf0f0e0abfa1a00f3
Dominic Seithel <seitheldominic@gmail.com>
2023-05-05 11:15:13 +0200

    reproducable randomness

--- src/helpers.ts
+++ src/helpers.ts
@@ -4,16 +4,21 @@
  * @returns Shuffled array
  */
 function G_shuffleNumberArray(array: Array<number>): Array<number> {
-    let m = array.length;
-    let t;
-    let i;
-
-    while (m) {
-        i = Math.floor(Math.random() * m--);
-        t = array[m];
-        array[m] = array[i];
-        array[i] = t;
+    let currentIndex = array.length;
+    let temporaryValue: number;
+    let randomIndex: number;
+    let seed = config.shuffleSeed;
+    seed = seed || 1;
+    let random = () => {
+        let x = Math.sin(seed++) * 10000;
+        return x - Math.floor(x);
+    };
+    while (0 !== currentIndex) {
+        randomIndex = Math.floor(random() * currentIndex);
+        currentIndex--;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
     }
-
     return array;
 }
